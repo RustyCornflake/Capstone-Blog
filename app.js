8fb0dd3eb7324ece13c31c3fe3c6aabf686dcfd9
RustyCornflake <sheldonroberts918@gmail.com>
2025-06-27 07:42:29 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+
 
 let posts = [];
 let idCounter = 1;
